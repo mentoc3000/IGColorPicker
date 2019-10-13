f3bae0bcd37b098353c37c2342a2113e5b198677
JP Sheehan <jp.sheehan2@gmail.com>
2019-10-13 09:33:33 -0400

    Uncheck old color when switching programmatically

--- IGColorPicker/Classes/View/ColorPickerView.swift
+++ IGColorPicker/Classes/View/ColorPickerView.swift
@@ -132,6 +132,10 @@ open class ColorPickerView: UIView, UICollectionViewDelegate, UICollectionViewDa
                 }
                 return
             }
+
+            if let oldIdx = _indexOfSelectedColor, let oldCell = collectionView.cellForItem(at: IndexPath(item: oldIdx, section: 0)) as? ColorPickerCell {
+                oldCell.checkbox.setCheckState(.unchecked, animated: animated)
+            }
             
             _indexOfSelectedColor = indexPath.item
             
